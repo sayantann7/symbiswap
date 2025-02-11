fb40e2bf96ac60973ff8fe6828bcfa0714f673b4
Sayantan Nandi <officialsayantannandi@gmail.com>
2025-02-11 17:39:51 +0530

    Added profile button in FeedNavbar

--- components/FeedNavbar.js
+++ components/FeedNavbar.js
@@ -39,6 +39,27 @@ function FeedNavbar() {
             </button>
           </Link>
 
+          <Link href="/profile">
+            <button
+              type="button"
+              className="relative inline-flex items-center p-2 text-sm font-medium text-center text-white mr-5 bg-gradient-to-br from-purple-600 to-blue-500 rounded-lg shadow-lg shadow-blue-500/50 dark:shadow-blue-800/80 hover:from-purple-700 hover:to-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800"
+            >
+              <svg
+                className="h-7 w-7 text-white-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </button>
+          </Link>
+
           <UserButton
             appearance={{
               elements: {
